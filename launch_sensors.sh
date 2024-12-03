0aa0a231d4d08ca430fc245f1d47bfc8821ae1b4
tlpss <thomas17.lips@gmail.com>
2024-12-03 16:11:40 +0100

    re-added proper launch_sensors.sh

--- launch_sensors.sh
+++ launch_sensors.sh
@@ -1,5 +1,6 @@
 
-gnome-terminal --tab -- bash -c "source /home/tlips/miniconda3/bin/activate && conda activate gello-pt  && cd /home/tlips/Code/SensorCommDDS/sensor_comm_dds && python -m communication.readers.irtouch32_ble_reader"
+gnome-terminal --tab -- bash -c "source /home/tlips/miniconda3/bin/activate && conda activate gello-pt  && cd /home/tlips/Code/SensorCommDDS/sensor_comm_dds && python -m communication.readers.switches_ble_reader"
+gnome-terminal --tab -- bash -c "source /home/tlips/miniconda3/bin/activate && conda activate gello-pt  && cd /home/tlips/Code/SensorCommDDS/sensor_comm_dds && python -m visualisation.visualisers.switches_visualiser Switches"
 sleep 1
-gnome-terminal --tab -- bash -c "source /home/tlips/miniconda3/bin/activate && conda activate gello-pt  && cd /home/tlips/Code/SensorCommDDS/sensor_comm_dds && python -m communication.readers.accelnet_ble_reader"
-gnome-terminal --tab -- bash -c "source /home/tlips/miniconda3/bin/activate && conda activate gello-pt  && cd /home/tlips/Code/SensorCommDDS/sensor_comm_dds && python -m visualisation.visualisers.irtouch32_visualiser IRTouch32"
+gnome-terminal --tab -- bash -c "source /home/tlips/miniconda3/bin/activate && conda activate gello-pt  && cd /home/tlips/Code/SensorCommDDS/sensor_comm_dds && python -m communication.readers.micmanip_serial_reader"
+gnome-terminal --tab -- bash -c "source /home/tlips/miniconda3/bin/activate && conda activate gello-pt  && cd /home/tlips/Code/SensorCommDDS/sensor_comm_dds && python -m visualisation.visualisers.micmanip_spectrogram_visualiser MicManip"
